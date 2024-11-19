903cffc4b018d0fa5bb9655b5c14306d71454a25
xdavide9 <biancodavidetech@gmail.com>
2024-11-19 11:45:04 +0100

    set up
    core spring and spring boot definitions

---
+++ src/test/java/com/xdavide9/springbootmasterclass/SpringbootMasterclassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xdavide9.springbootmasterclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMasterclassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
